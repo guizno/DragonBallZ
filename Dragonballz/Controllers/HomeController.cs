a4dd28c2e9abc3178b3b1aa8b115f8d63a8b1f27
Guilherme Henrique <guizinho.zaza14@gmail.com>
2023-05-23 08:40:53 -0300

    Alterando código do layout

--- Dragonballz/Controllers/HomeController.cs
+++ Dragonballz/Controllers/HomeController.cs
@@ -1,23 +1,33 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Dragonballz.Models;
+using Dragonballz.Services;
 
 namespace Dragonballz.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+     private readonly ILogger<HomeController> _logger;
+     private readonly IDragonService _dragonService;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+        _dragonService = dragonService;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(string especie)
     {
-        return View();
+        var especies = _dragonService.GetDragonballzDto();
+        ViewData["filter"] = string.IsNullOrEmpty(especie) ? "all" : especie;
+        return View(especie);
     }
 
+    public IActionResult Details(int Numero)
+    {
+        var guerreiro = _dragonService.GetDetailedGuerreiro(Numero);
+        return View(guerreiro);
+    }
     public IActionResult Privacy()
     {
         return View();
